c8577a627cebc045951f43f1ad49034ff9d4285f
Heriberto Hernandez Torres <heribertolord@gmail.com>
2023-11-23 15:43:47 -0600

    not fixed

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        User::create([
+            "name"=> "lars",
+            "email"=> "goetia@mimail.com",
+            "password"=> bcrypt("password"),
+        ]);
+        User::create([
+            "name"=> "heri",
+            "email"=> "heri@mimail.com",
+            "password"=> bcrypt("password"),
+        ]);
+    }
+}
